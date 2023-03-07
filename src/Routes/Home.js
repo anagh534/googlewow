0276dc514461af591a67e56a10c7c15a5a12a511
SatyaPrakashPalai <sp8368755@gmail.com>
2023-03-08 00:06:17 +0530

    changed the asset path to public and made the button

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -2,13 +2,15 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import AboutSection from '../components/AboutSection'
 import Navbar from '../components/Navbar'
+import Button from '../components/Button'
 
 export default function Home() {
   return (
     <div>
-        <Navbar />
-        <HeroSection />
-        <AboutSection />
+        {/* <Navbar /> */}
+        <Button/>
+        {/* <HeroSection />
+        <AboutSection /> */}
     </div>
   )
 }
---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import "./Button.css"
+
+function Button() {
+    
+  return (
+    <div className="button">
+        <p style={{marginRight:'17px'}}>Register Now</p> 
+        <img className="arrow" src= '/svgs/arrow.svg' />
+    </div>
+  )
+}
+
+export default Button
